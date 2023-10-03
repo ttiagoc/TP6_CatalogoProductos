54e76fa6397d182310f62e62fd597186e11255f7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-03 10:23:25 -0300

    localstorage

--- src/context/CarritoContext.js
+++ src/context/CarritoContext.js
@@ -1,22 +1,42 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const CarritoContext = createContext();
 
 const CarritoProvider = (props) => {
 
+
     const [productos, setProductos] = useState([]);
     const cantidadProductos = productos.length
 
+    useEffect(() => {
+        
+      if (localStorage.getItem("productos") != null) {
+        let storage = localStorage.getItem("productos")
+        setProductos(JSON.parse(storage))
+    }
+      },[])
+
+  
+    
+    
     const AddProduct = (producto) => {
-        setProductos([...productos,
-            producto])
+        setProductos([...productos, producto])
+        
+         addLocalStorage()
+    }
+
+    const addLocalStorage = () => {
+
+      localStorage.setItem("productos",JSON.stringify(productos))
+
     }
 
     const ResetCarrito = () => {
         setProductos([])
     }
+   
 
-
+  
     return (
         <CarritoContext.Provider
           value={{
